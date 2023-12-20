2ba5cbfcc19ee6e7c9b2c543ce58c8ed831b3954
CodeSandbox <support@codesandbox.io>
2023-12-20 03:31:57 +0000

    Sandbox snapshot commit

---
+++ src/components/Vault.vue
@@ -0,0 +1,29 @@
+<script>
+import { Vault } from "@dhx/trial-vault";
+import "@dhx/trial-vault/codebase/vault.min.css";
+
+export default {
+  props: ["data"],
+  data() {
+    return {
+      vault: null,
+    };
+  },
+
+  mounted() {
+    this.vault = new Vault(this.$refs.cont, {});
+    this.vault.data.parse(this.data);
+  },
+  unmounted() {
+    this.vault.destructor();
+    this.$refs.cont.innerHTML = "";
+  },
+};
+</script>
+
+<template>
+  <div
+    ref="cont"
+    style="width: '440px', height: '400px', margin: '50px auto'"
+  ></div>
+</template>
---
+++ src/data.js
@@ -0,0 +1,28 @@
+export function getData() {
+  return [
+    {
+      size: "100000",
+      name: "index.php",
+    },
+    {
+      size: "25555",
+      name: "index.js",
+    },
+    {
+      size: "2555412",
+      name: "document.doc",
+    },
+    {
+      size: "52555",
+      name: "documentation.pdf",
+    },
+    {
+      size: "23555",
+      name: "archive.zip",
+    },
+    {
+      size: "72555",
+      name: "prototype.psd",
+    },
+  ];
+}
